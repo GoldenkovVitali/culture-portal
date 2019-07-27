43b8c507b64e28dcd9bdf4128439d825de3171bb
uniorunr <gagael23@live.com>
2019-07-27 11:26:04 +0300

    #9/fix: remove quotes in aside section of Main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const Main = () => (
   <div className="main-wrapper">
     <header className="header-section">header</header>
     <main className="main-section">portal description</main>
-    <aside className="aside-section">'author of the day' block</aside>
+    <aside className="aside-section">author of the day block</aside>
     <footer className="footer-section">footer</footer>
   </div>
 );
