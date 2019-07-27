271732a80ef839782ad8af55cea52d3969b400f5
darya <daryanovak18@gmail.com>
2019-07-27 15:38:34 +0300

    refactor: add component to main page

---
+++ src/components/culturePortalInfo/culturePortalInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import './producerCard.scss';
+
+
+export default class CulturePortalInfo extends React.Component {
+  render() {
+    return (
+      <div className="portal-info">
+
+      </div>
+    );
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,21 @@ import ProducerCard from '../components/producerCard/producerCard';
 const Main = () => (
   <div className="main-wrapper">
     <header className="header-section">header</header>
-    <main className="main-section">portal description</main>
-    <ProducerCard/>
-    <aside className="aside-section">author of the day block</aside>
+    <main className="main-section">
+      <h3>CULTURE PORTAL</h3>
+      <article>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc libero magna, venenatis quis aliquet et,
+      rutrum in augue. Donec vel tempor dolor. Donec volutpat fringilla porta. Suspendisse non nulla tortor.
+      Quisque commodo ornare mi, sit amet aliquet justo bibendum non. Integer bibendum convallis sapien, sit
+      amet tincidunt orci placerat in. Integer vitae consequat augue.
+      </article>
+    </main>
+    <aside className="aside-section">
+      <div>
+      <h3>PRODUCER OF THE DAY</h3>
+      <ProducerCard />
+      </div>
+    </aside>
     <footer className="footer-section">footer</footer>
   </div>
 );
