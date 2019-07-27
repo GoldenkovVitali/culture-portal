776cf2cf19789bb586334f5a050e0c13e3dbcbf2
uniorunr <gagael23@live.com>
2019-07-27 11:23:36 +0300

    #9/refactor: remove return keyword in Main component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import '../scss/generic.scss';
 import '../scss/main.scss';
 
-const Main = () => {
-  return (
-    <div className="main-wrapper">
-      <header className="header-section">header</header>
-      <main className="main-section">portal description</main>
-      <aside className="aside-section">'author of the day' block</aside>
-      <footer className="footer-section">footer</footer>
-    </div>
-  )
-}
+const Main = () => (
+  <div className="main-wrapper">
+    <header className="header-section">header</header>
+    <main className="main-section">portal description</main>
+    <aside className="aside-section">'author of the day' block</aside>
+    <footer className="footer-section">footer</footer>
+  </div>
+);
 
 export default Main;
