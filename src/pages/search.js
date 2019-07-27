8acd59e7a9111170d5c87afd28602a4057042e16
uniorunr <gagael23@live.com>
2019-07-27 11:26:52 +0300

    #9/feat: add SearchPage component with initial markup

---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/generic.scss';
+import '../scss/search.scss';
+
+const SearchPage = () => (
+  <div className="search-wrapper">
+    <header className="header-section">header</header>
+    <section className="search-section">search bar</section>
+    <main className="main-section">cards</main>
+  </div>
+);
+
+export default SearchPage;
