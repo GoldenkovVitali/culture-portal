0e223cfabeef15773399601645d67f9b8dd6a88d
darya <daryanovak18@gmail.com>
2019-07-27 15:11:26 +0300

    fix: change font-size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../scss/generic.scss';
 import '../scss/main.scss';
+import ProducerCard from '../components/producerCard/producerCard';
 
 const Main = () => (
   <div className="main-wrapper">
     <header className="header-section">header</header>
     <main className="main-section">portal description</main>
+    <ProducerCard/>
     <aside className="aside-section">author of the day block</aside>
     <footer className="footer-section">footer</footer>
   </div>
