c935ac41d0a10c19728062f305d3f0ea2b18cbb1
SystemGhost <jackie07364538@gmail.com>
2019-07-30 23:14:55 +0300

    #46/feature: add data on main page

--- src/components/directorCard/directorCard.js
+++ src/components/directorCard/directorCard.js
@@ -1,26 +1,98 @@
 import React from 'react';
 import './directorCard.scss';
-
+import { StaticQuery, graphql, Link } from "gatsby";
 
 export default class DirectorCard extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { val: 1 };
+  }
+
   render() {
     return (
-      <div className="director-card">
-        <div className="director-card__image">
-          <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/fa48f282735235.5d266fe2f1b44.jpg" alt="director photo" />
-        </div>
-        <div className="director-card__info">
-            <div className="director-card__info-main">
-              <p>Yevgeniy Kryzhanovsky</p>
-              <p className="director-card__info-birth">6 June 1955 - xxx</p>
+      <StaticQuery
+        query={graphql`
+        query {
+          allContentfulPerson {
+            edges {
+              node {
+                id
+                idPage
+                nameRu
+                nameBy
+                nameEn
+                yearsOfLife
+                img {
+                  file {
+                    url
+                  }
+                }
+                cityRu
+                cityBy
+                cityEn
+                descriptionRu
+                descriptionBy
+                descriptionEn
+                timelineRu {
+                  date
+                  info
+                }
+                timelineBy {
+                  date
+                  info
+                }
+                timelineEn {
+                  date
+                  info
+                }
+                worksRu {
+                  date
+                  info
+                }
+                worksBy {
+                  date
+                  info
+                }
+                worksEn {
+                  date
+                  info
+                }
+                gallery {
+                  file {
+                    url
+                  }
+                }
+                map
+                youtube
+              }
+            }
+          }
+        }
+      `}
+        render={data => (
+          <header>
+            <h1>{JSON.stringify(data.allContentfulPerson.edges[this.state.val].node.descriptionRu)}</h1>
+
+            <div className="director-card">
+              <div className="director-card__image">
+                <img src={data.allContentfulPerson.edges[this.state.val].node.img.file.url} alt="director photo" />
+              </div>
+              <div className="director-card__info">
+                <div className="director-card__info-main">
+                  <p>{data.allContentfulPerson.edges[this.state.val].node.nameRu}</p>
+                  <p className="director-card__info-birth">{data.allContentfulPerson.edges[0].node.yearsOfLife}</p>
+                </div>
+                <hr align="center" width="80" size="0.5" />
+                <div className="director-card__info-description">
+                  <p>{data.allContentfulPerson.edges[this.state.val].node.descriptionRu}</p>
+                </div>
+                <Link className="director-card__learn-more-button" to={'/author'} state={data.allContentfulPerson.edges[this.state.val].node}><span>learn more</span></Link>
+              </div>
             </div>
-          <hr align="center" width="80" size="0.5" />
-          <div className="director-card__info-description">
-            <p>Born in 1955, in Nikolayev (SSR). He graduated from the school in Kozelsk ( RSFSR ) in 1972 year. After the failure of the Moscow Theater School, he worked in the Tula Drama Theater stagehand. In 1973 he entered the Belarusian Theater and Art Institute in the acting department.</p>
-          </div>
-          <button className="director-card__learn-more-button" type="button"><span>learn more</span></button>
-        </div>
-      </div>
+          </header>
+        )}
+      />
     );
   }
 }
