2223004ffe87f3c6c533a236b7ed8a6f5a342313
EDovgalo <edovgalo@gmail.com>
2019-07-30 23:49:56 +0300

    feat: add style for photo-gallery

--- src/components/photoGallery/index.js
+++ src/components/photoGallery/index.js
@@ -3,19 +3,27 @@ import PropTypes from 'prop-types';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import './style.scss';
+
 import imageOne from '../../../static/1.jpg';
 import imageTwo from '../../../static/2.jpg';
 import imageThree from '../../../static/3.jpg';
 import imageFour from '../../../static/4.jpg';
 
-const PhotoGallery = ({ arrImageUrl = [imageOne, imageTwo, imageThree, imageFour] }) => {
+const PhotoGallery = ({
+  arrImageUrl = [imageOne, imageTwo, imageThree, imageFour, imageFour],
+}) => {
   const images = arrImageUrl.map(item => (
     <div>
       <img src={item} alt="icon of work" />
     </div>
   ));
 
-  return <Carousel>{images}</Carousel>;
+  return (
+    <div className="photo-gallery">
+      <Carousel>{images}</Carousel>
+    </div>
+  );
 };
 
 PhotoGallery.propTypes = {
