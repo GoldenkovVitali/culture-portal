c25080e055942e726a84909048dc85cb826f2c9b
Anton Yegorov <an_tosha33@mail.ru>
2019-07-30 21:01:38 +0300

    #21/feature: test throwing data in timeline

--- src/pages/author.js
+++ src/pages/author.js
@@ -4,6 +4,19 @@ import '../scss/author.scss';
 import Timeline from '../components/timeline/TimelineContainer';
 import Map from '../components/map/map';
 
+const data = {
+  index1: 'adadasd',
+  index2: 'afklalkalakjg',
+  index3: 'asdlkadl;kad;l',
+  index4: 'asdlkadl;kad;l',
+  index5: 'asdlkadl;kad;l',
+  index6: 'asdlkadl;kad;l',
+  index10: 'asdlkadl;kad;l',
+  index7: 'asdlkadl;kad;l',
+  index8: 'asdlkadl;kad;l',
+  index9: 'asdlkadl;kad;l',
+};
+
 const AuthorPage = () => (
   <div className="author-wrapper">
     <header className="header-section">header</header>
@@ -11,7 +24,7 @@ const AuthorPage = () => (
     <main className="main-section">
       <section className="biography">biography</section>
       <section className="timelineContainer" id="timeline">
-        <Timeline />
+        <Timeline data={data} />
       </section>
       <section className="gallery">gallery</section>
       <Map />
