faf5b1f544c08e15d6168baea2e9c915308d3a02
Anton Yegorov <an_tosha33@mail.ru>
2019-07-31 18:09:28 +0300

    #48/feature: add video overlay and button

--- src/pages/author.js
+++ src/pages/author.js
@@ -3,21 +3,25 @@ import '../scss/generic.scss';
 import '../scss/author.scss';
 import Map from '../components/map/map';
 import Gallery from '../components/photoGallery';
+import ModalVideoContainer from '../components/videoOverlay/videoOverlay';
 
 const AuthorPage = (data) => {
-  console.log('data', data);    // data in data.location.state
+  console.log('data', data); // data in data.location.state
   return (
     <div className="author-wrapper">
       <header className="header-section">header</header>
       <aside className="author-card-section">author card</aside>
       <main className="main-section">
-        <section className="biography">biography</section>
+        <section className="biography">
+          biography
+          <ModalVideoContainer link={data.location.state.youtube} />
+        </section>
         <section className="timeline">timeline</section>
         <section className="gallery"><Gallery /></section>
         <Map content={data.location.state.map} />
       </main>
     </div>
   );
-}
+};
 
 export default AuthorPage;
