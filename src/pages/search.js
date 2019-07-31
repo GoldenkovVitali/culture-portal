a9c24116cb151be218f0f160fc0d24c62f3347f2
uniorunr <gagael23@live.com>
2019-07-31 14:02:32 +0300

    #44/feature: add initial functionality for search page (filter cards on search input)

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,30 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link, graphql } from 'gatsby';
+import SearchInput, { createFilter } from 'react-search-input';
 import '../scss/generic.scss';
 import '../scss/search.scss';
 
-const SearchPage = ({ data }) => {
-  console.log(data);
+const KEYS_TO_FILTERS = ['node.nameRu'];
 
-  return (
-    <Fragment>
-      <section className="search-section">search bar</section>
-      <section className="list-section">
-        <h2>Genereate Links by GraphQl:</h2>
-        <ul>
-          {
-            data.allContentfulPerson.edges.map(({ node }) => (
-              <li key={node.idPage}>
-                <Link to="/author" state={node}>{node.nameRu}</Link>
-              </li>
-            ))
-            }
-        </ul>
-      </section>
-      <main className="main-section">cards</main>
-    </Fragment>
-  );
-};
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.searchUpdated = this.searchUpdated.bind(this);
+  }
+
+  searchUpdated = (term) => {
+    this.setState({ searchTerm: term });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { searchTerm } = this.state;
+    const filteredEmails = data
+      .allContentfulPerson
+      .edges.filter(createFilter(searchTerm, KEYS_TO_FILTERS));
+
+    return (
+      <Fragment>
+        <section className="search-section">
+          <SearchInput className="search-input" onChange={this.searchUpdated} />
+        </section>
+        <section className="main-section">
+          <h2>Genereate Links by GraphQl:</h2>
+          <ul>
+            {
+              filteredEmails.map(({ node }) => (
+                <li key={node.idPage}>
+                  <Link to="/author" state={node}>{node.nameRu}</Link>
+                </li>
+              ))
+             }
+          </ul>
+        </section>
+      </Fragment>
+    );
+  }
+}
 
 export default SearchPage;
 
