56ef7865557f4e2f947e3a8d7b0367c6d6a18731
SystemGhost <jackie07364538@gmail.com>
2019-08-01 12:40:40 +0300

    #53/fix: fix data on authoe page

--- src/pages/author.js
+++ src/pages/author.js
@@ -4,12 +4,41 @@ import '../scss/author.scss';
 import Map from '../components/map/map';
 import Gallery from '../components/photoGallery';
 import Page404 from '../components/page404/page404';
+import { setI18n } from 'react-i18next';
 
-const AuthorPage = (data) => {
-  console.log('data', data);    // data in data.location.state
+const AuthorPage = (_data) => {
+  console.log('data', _data);    // data in data.location.state
+
+  let storage = JSON.parse(localStorage.getItem('culture-author'));
+  let data = null;
+
+  if (_data && _data.location && _data.location.state && _data.location.state.idPage) {
+    if (storage) {
+      if (storage.location && storage.location.state && storage.location.state.idPage) {
+        if (storage.location.state.idPage !== _data.location.state.idPage) {
+          localStorage.setItem('culture-author', JSON.stringify(_data));
+          data = _data;
+        } else {
+          data = storage;
+        }
+      } else {
+        data = _data;
+      }
+    } else {
+      localStorage.setItem('culture-author', JSON.stringify(_data));
+      data = _data;
+    }
+  } else {
+    if (storage) {
+      if (storage.location && storage.location.state && storage.location.state.idPage) {
+        data = storage;
+      }
+    }
+  }
+
   return (
     <div>
-      {data.location.state ? <div className="author-wrapper">
+      {data && data.location.state ? <div className="author-wrapper">
         <header className="header-section">header</header>
         <aside className="author-card-section">author card</aside>
         <main className="main-section">
