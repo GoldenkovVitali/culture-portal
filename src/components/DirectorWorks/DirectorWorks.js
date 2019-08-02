04e750f6c2b71c2214ef362b30afd0a44616b252
uniorunr <gagael23@live.com>
2019-08-03 01:48:50 +0300

    #31/feat: fully implement DirectorWorks component

--- src/components/DirectorWorks/DirectorWorks.js
+++ src/components/DirectorWorks/DirectorWorks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Trans } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -6,59 +7,59 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import { greyColor } from '../../scss/colors.scss';
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
     overflowX: 'auto',
+  },
+  head: {
+    background: greyColor,
+  },
+  headCell: {
+    fontFamily: '\'Playfair Display\', serif',
+    fontSize: '18px',
+  },
+  bodyCell: {
+    fontFamily: '\'Raleway\', sans-serif',
   },
   table: {
     minWidth: 650,
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function SimpleTable() {
+const DirectorWorks = ({ data, lang }) => {
   const classes = useStyles();
+  const langKey = `works${lang.charAt(0).toUpperCase()}${lang.slice(1)}`;
+  const rows = data[langKey];
 
   return (
     <Paper className={classes.root}>
       <Table className={classes.table}>
-        <TableHead>
+        <TableHead className={classes.head}>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell className={classes.headCell}><Trans>Year of creation</Trans></TableCell>
+            <TableCell align="right" className={classes.headCell}>
+              <Trans>Works</Trans>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
+            <TableRow key={row.info.split('').slice(0, 10).join('')}>
+              <TableCell component="th" scope="row" className={classes.bodyCell}>
+                {row.date}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right" className={classes.bodyCell}>{row.info}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </Paper>
   );
-}
+};
+
+export default DirectorWorks;
